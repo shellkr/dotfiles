4cca8fae61726b794023867d25eb7936f02804a0
Daniel Sandman <revoltism@gmail.com>
2013-07-02 22:21:18 +0200

    updated weather and conkyrc

--- home/.config/i3/weather.sh
+++ home/.config/i3/weather.sh
@@ -1,6 +1,12 @@
 #!/bin/zsh
 
-winfo=$(curl -s http://www.yr.no/place/Sweden/Norrbotten/Ala_Lombolo/forecast.xml | egrep '(temperature|symbol)' | \
+## The temperature is fetched from the free Norwegian forcast service Yr.no. Change the wurl variable to
+## correspond with your location. Your ip can be blocked if you try to fetch the forecast too quick. So
+## use an interval longer than 10 minutes.
+
+wurl="http://www.yr.no/place/Sweden/Norrbotten/Ala_Lombolo/forecast.xml"
+
+winfo=$(curl -s $wurl | egrep '(temperature|symbol)' | \
 awk -F'="|"' '{printf $4","}' | cut -d "," -f 1-4)
 
 awk -F"," '{print $2"°C",$1" | "$4"°C",$3}' <<<$winfo
