fbb3e666332e83493db5ac850fcdef0f93b060d7
Daniel Sandman <revoltism@gmail.com>
2013-07-09 07:34:55 +0200

    "some small changes..."

--- home/.config/i3/weather.sh
+++ home/.config/i3/weather.sh
@@ -9,9 +9,10 @@ wurl="http://www.yr.no/place/Sweden/Norrbotten/Ala_Lombolo/forecast.xml"
 winfo=$(curl -s $wurl | egrep '(temperature|symbol)' | \
 awk -F'="|"' '{printf $4","}' | cut -d "," -f 1-4)
 
+[ "grep -i '^Cloudy$' <<<$winfo" ] && winfo=$(sed 's/Cloudy/\\u2601/g' <<<"$winfo")
 [ "grep -i '^Partly cloudy$' <<<$winfo" ] && winfo=$(sed 's/Partly cloudy/\\u2601/g' <<<"$winfo")
 [ "grep -i '^Fair$' <<<$winfo" ] && winfo=$(sed 's/Fair/\\u2600/g' <<<"$winfo")
 [ "grep -i '^Rain$' <<<$winfo" ] && winfo=$(sed 's/Rain/\\u2602/g' <<<"$winfo")
-[ "grep -i '^Heavy Rain$' <<<$winfo" ] && winfo=$(sed 's/Heavy Rain/\\u2614/g' <<<"$winfo")
+[ "grep -i '^Heavy rain$' <<<$winfo" ] && winfo=$(sed 's/Heavy rain/\\u2614\\u26a0/g' <<<"$winfo")
 
 awk -F"," '{print $2"°C",$1"  "$4"°C",$3}' <<<$winfo
