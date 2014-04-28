2f6c199a3495a6ad8182a6fa4a5896b927412e7b
Daniel Sandman <revoltism@gmail.com>
2014-04-28 23:43:47 +0200

    "added Snapwm and did some changes in Dwb"

--- home/.config/i3/hot_edge.sh
+++ home/.config/i3/hot_edge.sh
@@ -3,7 +3,9 @@
 echo "started $0"
 
 xdotool behave_screen_edge --delay 500 left key Alt+1 & left_edge=$!
+#xdotool behave_screen_edge --delay 500 left key Super+left & left_edge=$!
 xdotool behave_screen_edge --delay 500 right key Alt+2 & right_edge=$!
+#xdotool behave_screen_edge --delay 500 right key Super+right & right_edge=$!
 
 sleep 10m
 
