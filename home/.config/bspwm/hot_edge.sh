fe4154b0d63c1120bd6a3dd38dfe72f1bd5ebc3b
Daniel Sandman <revoltism@gmail.com>
2015-02-25 10:22:30 +0100

    "Will use hot-corner insted of edge and switched Firefox to Dwb to autostart"

--- home/.config/bspwm/hot_edge.sh
+++ home/.config/bspwm/hot_edge.sh
@@ -4,9 +4,9 @@ echo "started $0"
 
 while true
 do
-	xdotool behave_screen_edge --delay 500 left key Alt+1 & left_edge=$!
-	xdotool behave_screen_edge --delay 500 right key Alt+2 & right_edge=$!
-	sleep 10m
+	xdotool behave_screen_edge --delay 500 bottom-left key Alt+1 & left_edge=$!
+	xdotool behave_screen_edge --delay 500 bottom-right key Alt+2 & right_edge=$!
+	sleep 5m
 	kill $right_edge $left_edge
 done
 
