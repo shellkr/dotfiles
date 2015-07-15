3371e0d77acf5b43f0861885b9968fbf19c0531d
Almehdi <revoltism@gmail.com>
2015-07-15 02:56:49 +0200

    ...

--- home/.config/bspwm/hot_edge.sh
+++ home/.config/bspwm/hot_edge.sh
@@ -4,8 +4,8 @@ echo "started $0"
 
 while true
 do
-	xdotool behave_screen_edge --delay 500 bottom-left key Alt+1 & left_edge=$!
-	xdotool behave_screen_edge --delay 500 bottom-right key Alt+2 & right_edge=$!
+	xdotool behave_screen_edge --delay 500 bottom-left key Alt+Shift+1 & left_edge=$!
+	xdotool behave_screen_edge --delay 500 bottom-right key Alt+Shift+2 & right_edge=$!
 	sleep 5m
 	kill $right_edge $left_edge
 done
